79c5cebdde6c15357f8ad344754bd9ee4a696628
Meriles <66324021+chibuezej@users.noreply.github.com>
2022-11-08 20:47:09 +0000

    display

--- src/Components/500.jsx
+++ src/Components/500.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+
 export default function Error(props){
     const cardStyle = {
         "borderRadius" : "10px",
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -4,7 +4,7 @@ import { NavLink } from "react-router-dom";
 export default function Home() {
   return (
     <div id="home">
-      <h1>Home Page</h1>
+      <h1 className="">Home Page</h1>
      <h2>Alt-school-exam</h2>
       <h3 className="name">Nmeregini C Johnhenry</h3>
       <p className="paragh">Setup a custom counter hook with increment, decrement, reset, setValue functions with a valid UI and Implement a combination of states with a useReducer that implements a counter with the four evident features in the custom hook -  increment, decrement, reset, setValue. Implement a page for the custom hook, useReducer, 404, and a page to test error boundary and good SEO.</p>
