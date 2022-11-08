e778ffe2b12d7fe4b3654220dff3467574f46682
Meriles <66324021+chibuezej@users.noreply.github.com>
2022-11-08 00:39:32 +0000

    500

--- src/Components/Error.jsx
+++ src/Components/Error.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import Error from "./500";
 
 class ErrorBoundary extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { error: null, errorInfo: null };
+ 
+    state = { error: null, errorInfo: null };
+  
+static getDerivedStateFromError(error) {
+    // Update state so the next render will show the fallback UI.
+    return { errorInfo: true };
   }
-
+  
   componentDidCatch(error, errorInfo) {
     this.setState({
       error: error,
@@ -16,14 +20,8 @@ class ErrorBoundary extends React.Component {
   render() {
     if (this.state.errorInfo) {
       return (
-        <div>
-          <h2>An Error Has Occurred</h2>
-          <details>
-            {this.state.error && this.state.error.toString()}
-            <br />
-            {this.state.errorInfo.componentStack}
-          </details>
-        </div>
+        <Error/>
+
       );
     }
 
--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom';
+import ErrorBoundary from './Components/Error'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ErrorBoundary>
+
+         <App />
+      </ErrorBoundary>
+     
     </BrowserRouter>
   </React.StrictMode>
 )
